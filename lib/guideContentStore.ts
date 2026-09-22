66bdeef184ea431a1f12c900d21bcc65a74708b9
fathimathasmaa-sudo <fathimathasmaa@gmail.com>
2026-09-22 10:11:49 +0500

    Revert failed Firestore client store change

--- lib/guideContentStore.ts
+++ lib/guideContentStore.ts
@@ -1,7 +1,7 @@
-import { doc, getDoc, setDoc } from "firebase/firestore";
-import { firestoreDb } from "./firebase";
+import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
+import { firebaseApp } from "./firebase";
 
-export const guideContentRef = doc(firestoreDb, "guideContent", "main");
+export const guideContentRef = doc(getFirestore(firebaseApp), "guideContent", "main");
 
 const FIREBASE_TIMEOUT_MS = 10000;
 
